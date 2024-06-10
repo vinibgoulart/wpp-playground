e3259d50cb17a090bb5e53d200bf0d2088d2fdc4
Akinn Rosa <akinncar@hotmail.com>
2024-06-10 10:08:50 -0300

    feat: meme trade offer

---
+++ src/wweb/onMessage/meme/tradeOfferMessage.ts
@@ -0,0 +1,39 @@
+import { Message, MessageMedia } from 'whatsapp-web.js';
+import { imgflipCaption } from '../../../imgflip/imgflipCaption';
+import imageToBase64 from 'image-to-base64';
+import { middleware } from '../../middleware/middleware';
+
+const tradeOfferMessage = async (msg: Message) => {
+  if (!msg.body) {
+    return;
+  }
+
+  const [text0, text1] = msg.body
+    .replace('!trade-offer', '')
+    .split('-')
+    .map((text) => text.trim());
+
+  if (!text0 || !text1) {
+    return;
+  }
+
+  const imgFlipCaptionResult = await imgflipCaption({
+    randomTemplatedId: '309868304',
+    text0,
+    text1,
+  });
+
+  if (!imgFlipCaptionResult.success) {
+    return msg.reply(imgFlipCaptionResult.error);
+  }
+
+  const base64 = await imageToBase64(imgFlipCaptionResult.url);
+
+  const replyMedia = new MessageMedia('image/png', base64);
+
+  msg.reply(replyMedia, undefined, {
+    sendMediaAsSticker: true,
+  });
+};
+
+export default middleware(tradeOfferMessage);
