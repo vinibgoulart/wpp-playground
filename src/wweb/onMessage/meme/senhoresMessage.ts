f273cdf1a5385f26fdd7579d558d5b5932767bf9
Vinicius <viblaziusgoulart@gmail.com>
2024-06-12 09:04:42 -0300

    feat(senhores): add senhores meme sticker

---
+++ src/wweb/onMessage/meme/senhoresMessage.ts
@@ -0,0 +1,32 @@
+import { Message, MessageMedia } from 'whatsapp-web.js';
+import { imgflipCaption } from '../../../imgflip/imgflipCaption';
+import imageToBase64 from 'image-to-base64';
+import { middleware } from '../../middleware/middleware';
+
+const senhoresMessage = async (msg: Message) => {
+  const text1 = msg.body.replace('!senhores', '').trim();
+
+  if (!text1) {
+    return;
+  }
+
+  const imgFlipCaptionResult = await imgflipCaption({
+    randomTemplatedId: '342785297',
+    text0: 'senhores',
+    text1,
+  });
+
+  if (!imgFlipCaptionResult.success) {
+    return msg.reply(imgFlipCaptionResult.error);
+  }
+
+  const base64 = await imageToBase64(imgFlipCaptionResult.url);
+
+  const replyMedia = new MessageMedia('image/png', base64);
+
+  msg.reply(replyMedia, undefined, {
+    sendMediaAsSticker: true,
+  });
+};
+
+export default middleware(senhoresMessage);
