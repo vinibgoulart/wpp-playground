e9c0273eea0312c66ea3bf7d8b63c513f5de3aae
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2024-06-15 14:47:31 -0300

    feat(audio): cost by caracter (#58)

--- src/wweb/middleware/middleware.ts
+++ src/wweb/middleware/middleware.ts
@@ -8,13 +8,17 @@ import { config } from 'src/config';
 type MiddlewareOptions = {
   isListening?: boolean;
   cost?: number;
+  costByCaractere?: boolean;
+  command?: string;
   onlyOwner?: boolean;
 };
 
 export const middleware = (
   next: (msg: Message, preparedEvent: PreparedEvent) => unknown,
-  { isListening = false, cost = 0, onlyOwner = false }: MiddlewareOptions = {},
+  options: MiddlewareOptions = {},
 ) => {
+  const { isListening, cost, command, costByCaractere, onlyOwner } = options;
+
   return async (msg: Message, preparedEvent: PreparedEvent) => {
     // if onlyOwner is true, the message will only be processed if it is sent by the owner,
     // otherwise it will only be processed if it is not sent by the owner
@@ -28,7 +32,20 @@ export const middleware = (
     }
 
     if (cost) {
-      const { error } = await consumerCredits({ msg, preparedEvent, cost });
+      const getCost = () => {
+        if (costByCaractere) {
+          const body = msg.body.replace(`${command}`, '').trim();
+          return cost * body.length;
+        }
+
+        return cost;
+      };
+
+      const { error } = await consumerCredits({
+        msg,
+        preparedEvent,
+        cost: getCost(),
+      });
 
       if (error) {
         msg.reply(error);
